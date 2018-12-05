5a97c66a18a1a10b318fb625a45eb12ac797afb1
HoangPhu98 <hoangvanphu98@gmail.com>
2018-12-05 12:56:40 +0700

    update

---
+++ public/js/table.js
@@ -0,0 +1,8 @@
+let addTag = document.querySelectorAll(".add-tag")
+addTag.forEach(element => {
+   //event add input tag to create bar in .container_d 
+});
+
+document.getElementById("add-list").onclick = function(){
+    //add event create new list
+}
